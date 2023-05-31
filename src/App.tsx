8f5a62f74d2d6749dfd7778e854760ef0c1aa217
jzfraser <jackzfras@gmail.com>
2023-05-31 00:49:20 -0600

    Aesthetics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import "./App.css";
+
+import { About } from "./components/About";
 import { NavBar } from "./components/NavBar";
+import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <h1 className="text-center">Hello there</h1>
+      <About />
+      <Skills />
+      <Projects />
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,23 @@
-import { faTerminal } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 
 export function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <FontAwesomeIcon icon={faTerminal} color="purple" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Nav className="nav">
+      <Nav.Link className="navlink" href="#home">
+        Home
+      </Nav.Link>
+      <Nav.Link className="navlink" href="#about">
+        About
+      </Nav.Link>
+      <Nav.Link className="navlink" href="#home">
+        <h1 className="initials">jf</h1>
+      </Nav.Link>
+      <Nav.Link className="navlink" href="#skills">
+        Skills
+      </Nav.Link>
+      <Nav.Link className="navlink" href="#projects">
+        Projects
+      </Nav.Link>
+    </Nav>
   );
 }
